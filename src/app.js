011cc0ec6bd2405fb52644ed0a04b7f8db8293f8
Sourab Hossain <sourabhossain.fci@gmail.com>
2022-11-08 14:56:16 +0600

    DEV: #08 - Swagger Documentation

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
 const CONFIG = require("./config/config");
 const errorHandler = require("./helpers/errorHandler");
 const connectWithMongoDB = require("./config/db");
+const swaggerDocument = require("./../swagger.json");
 const apiV1 = require("./apiV1/index");
 require("dotenv").config();
 
@@ -12,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/v1", apiV1);
-
+app.use("/v1/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(errorHandler.notFound);
 app.use(errorHandler.internalServerError);
 
